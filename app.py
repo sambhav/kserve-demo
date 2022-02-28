4856fd76a361293c4ac26b60eb70d66bc2cfdb1b
Sambhav Kothari <sambhavs.email@gmail.com>
2022-02-28 19:21:33 +0000

    Add minimal files

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import kserve
 from typing import Dict
 
-class AlexNetModel(kserve.Model):
+class CustomModel(kserve.Model):
     def __init__(self, name: str):
        super().__init__(name)
        self.name = name
-       self.load()
-
-    def load(self):
-        pass
 
     def predict(self, request: Dict) -> Dict:
-        pass
+        return {"predictions": request.get("instances", [])}
 
 if __name__ == "__main__":
-    model = AlexNetModel("custom-model")
+    model = CustomModel("custom-model")
     kserve.ModelServer().start([model])
